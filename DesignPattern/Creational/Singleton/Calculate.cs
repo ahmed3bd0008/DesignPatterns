0d7c26cabcc69b9dab9ed51610d24ead3a202787
ahmed3bd0008 <ahmed3bd0008@gmail.com>
2022-03-30 16:58:41 +0200

    start

---
+++ DesignPattern/Creational/Singleton/Calculate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Calculate
+    {
+        private Calculate() { }
+        public int Count { get; set; }
+        //in start of programme will create instance of object until if not use instance
+       // private static Calculate calculate=new Calculate() ;
+
+
+        //only create instance when used it
+        private static Calculate calculate=null ;
+        public static Calculate  GetInstance(){
+            if(calculate==null)
+                    calculate=new Calculate();
+            return calculate;
+        }
+        public void AddOne(){
+            Count++;
+        }
+    }
+}
---
+++ DesignPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Singleton;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calculate1=Calculate.GetInstance();
+            calculate1.AddOne();
+           Calculate calculate2=Calculate.GetInstance();
+            calculate2.AddOne();
+            Console.WriteLine(calculate1.Count);
+            Console.WriteLine(calculate2.Count);
+            calculate1.AddOne();
+             Console.WriteLine(calculate1.Count);
+            Console.WriteLine(calculate2.Count);
+        }
+    }
+}
