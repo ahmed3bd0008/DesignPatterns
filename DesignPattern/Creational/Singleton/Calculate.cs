63c9981e4deeed91ecabd8c076b7a3b518d3aed6
ahmed3bd0008 <ahmed3bd0008@gmail.com>
2022-04-04 15:24:42 +0200

    add singlton thread save

--- DesignPattern/Creational/Singleton/Calculate.cs
+++ DesignPattern/Creational/Singleton/Calculate.cs
@@ -1,20 +1,29 @@
-﻿using System;
+﻿    using System;
 
 namespace Singleton
 {
     public class Calculate
     {
         private Calculate() { }
-        public int Count { get; set; }
+        public int Count { get; set; }=0;
         //in start of programme will create instance of object until if not use instance
        // private static Calculate calculate=new Calculate() ;
 
 
         //only create instance when used it
         private static Calculate calculate=null ;
+        private static object LockedObject=new Object();
         public static Calculate  GetInstance(){
-            if(calculate==null)
-                    calculate=new Calculate();
+            if (calculate == null)
+            {
+                lock (LockedObject)
+                {
+                    if (calculate == null)
+                    {
+                        calculate = new Calculate();
+                    }
+                }
+            }
             return calculate;
         }
         public void AddOne(){
--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Singleton;
 
 namespace DesignPattern
@@ -7,15 +8,26 @@ namespace DesignPattern
     {
         static void Main(string[] args)
         {
-            Calculate calculate1=Calculate.GetInstance();
-            calculate1.AddOne();
-           Calculate calculate2=Calculate.GetInstance();
-            calculate2.AddOne();
-            Console.WriteLine(calculate1.Count);
-            Console.WriteLine(calculate2.Count);
-            calculate1.AddOne();
-             Console.WriteLine(calculate1.Count);
-            Console.WriteLine(calculate2.Count);
+        //     Calculate calculate1=Calculate.GetInstance();
+        //     calculate1.AddOne();
+        //    Calculate calculate2=Calculate.GetInstance();
+        //     calculate2.AddOne();
+        //     Console.WriteLine(calculate1.Count);
+        //     Console.WriteLine(calculate2.Count);
+        //     calculate1.AddOne();
+        //      Console.WriteLine(calculate1.Count);
+        //     Console.WriteLine(calculate2.Count);
+            Task task1=Task.Factory.StartNew(()=>{
+                 Calculate calculate1=Calculate.GetInstance();
+                 calculate1.AddOne();
+                 Console.WriteLine($"calculte1{calculate1.Count}");
+            });
+            Task task2=Task.Factory.StartNew(()=>{
+                 Calculate calculate2=Calculate.GetInstance();
+                 calculate2.AddOne();
+                 Console.WriteLine($"calculte2{calculate2.Count}");
+            });
+            Console.ReadKey();
         }
     }
 }
